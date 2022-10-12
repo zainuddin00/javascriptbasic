4411ad49384aff796147fb6189c4880d3832cb07
zainuddin00 <zainfarezha@gmail.com>
2022-10-12 16:05:41 +0700

    javascriptbasic

---
+++ scrip.js
@@ -0,0 +1,24 @@
+let number = 20;
+let string = "Kampus merdeka";
+let object = {
+  nama: "Firstyan",
+  umur: 20,
+  kerja: false,
+};
+
+let array = [1, 2, 3, 4, 5]; 
+
+let boolean = true;
+
+
+let a, b, c;
+a = 20;
+b = false;
+c = "javascript";
+
+console.log(number);
+console.log(string);
+console.log(object);
+console.log(number);
+console.log(array);
+console.log(a, b, c);
